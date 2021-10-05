94c1967682b09a5518c48a5e2db7e63f98a816bf
Jeff Matocha <matochaj@obu.edu>
2021-10-05 12:07:52 -0500

    Three versions of rotating the cannon.

--- Scenes/Tank.gd
+++ Scenes/Tank.gd
@@ -16,9 +16,16 @@ func _process(delta):
 	var tank_position = position
 	var mouse_position = get_viewport().get_mouse_position()
 	var toward_mouse = (mouse_position - tank_position).normalized()
-	
-	var cos_theta = Vector2.RIGHT.dot(toward_mouse)
-	if mouse_position.y > tank_position.y:
-		$"Cannon Container".rotation = acos(cos_theta)
-	else:
-		$"Cannon Container".rotation = -acos(cos_theta)
+
+#	Version 1	
+#	var cos_theta = Vector2.RIGHT.dot(toward_mouse)
+#	if mouse_position.y > tank_position.y:
+#		$"Cannon Container".rotation = acos(cos_theta)
+#	else:
+#		$"Cannon Container".rotation = -acos(cos_theta)
+
+#	Version 2
+#	$"Cannon Container".rotation = Vector2.RIGHT.angle_to(toward_mouse)
+
+#	Version 3
+	$"Cannon Container".rotation = mouse_position.angle_to_point(tank_position)	
